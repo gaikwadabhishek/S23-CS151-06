7a99ec3de630144528ddd0db39e36f7a0ddab59b
rickynguyen12 <83847310+rickynguyen12@users.noreply.github.com>
2023-02-08 21:42:12 -0800

    Create NguyenTest.java

---
+++ src/hw1/NguyenTest.java
@@ -0,0 +1,11 @@
+package hw1;
+
+public class NguyenTest {
+	
+	public static void main(String[] args) {
+		Student myStudent = new Student("Ricky", "3rd year", "9181", "COMPUTER_SCIENCE", "06/12/02");
+		System.out.println(myStudent);
+
+	}
+
+}
