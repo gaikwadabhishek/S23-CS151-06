78f747d43412d787358673912b71e7773b37f97f
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-02-08 23:01:30 -0800

    Update NguyenTest.java

--- src/hw1/NguyenTest.java
+++ src/hw1/NguyenTest.java
@@ -1,9 +1,18 @@
+/*
+ * Hoang Nguyen
+ * Homework 1, part 1
+ * 
+ * Resources: 
+ * Date Class: https://www.geeksforgeeks.org/date-class-java-examples/
+ * Enum: https://www.geeksforgeeks.org/enum-in-java/
+ */
 package hw1;
 import java.util.*;
 public class NguyenTest {
 	
 	public static void main(String[] args) {
 		Branch major = Branch.SOFTWARE_ENGINEERING;
+		//Reference to geeksforgeeks Data class in Java
 		Date birthday = new Date("03/11/2004");
 		Student hoangNguyen = new Student("Hoang Nguyen", 
 				"My name is Hoang Nguyen", 3619, major, birthday);
