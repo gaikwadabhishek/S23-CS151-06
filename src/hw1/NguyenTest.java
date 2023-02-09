91e56c94dcdd8bd76c1bb6337c66254b3c9e5e6d
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-02-08 22:44:30 -0800

    Update NguyenTest.java

--- src/hw1/NguyenTest.java
+++ src/hw1/NguyenTest.java
@@ -1,1 +1,12 @@
+package hw1;
 
+public class NguyenTest {
+	
+	public static void main(String[] args) {
+		Branch major = Branch.SOFTWARE_ENGINEERING;
+		Date birthday = new Date(2004, 3, 11);
+		Student hoangNguyen = new Student("Hoang Nguyen", 
+				"My name is Hoang Nguyen", 3619, major, "03/11/2004");
+  }
+
+}
