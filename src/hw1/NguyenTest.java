6e5351dcf9bb1ddd841f7a0b4fcb1f73873e9ebe
Hoang Nguyen <115848843+Da0range@users.noreply.github.com>
2023-02-08 22:44:54 -0800

    Update NguyenTest.java

--- src/hw1/NguyenTest.java
+++ src/hw1/NguyenTest.java
@@ -1,12 +1,13 @@
 package hw1;
-
+import java.util.*;
 public class NguyenTest {
 	
 	public static void main(String[] args) {
 		Branch major = Branch.SOFTWARE_ENGINEERING;
-		Date birthday = new Date(2004, 3, 11);
+		Date birthday = new Date("03/11/2004");
 		Student hoangNguyen = new Student("Hoang Nguyen", 
-				"My name is Hoang Nguyen", 3619, major, "03/11/2004");
-  }
+				"My name is Hoang Nguyen", 3619, major, birthday);
+		System.out.println(hoangNguyen.toString());
+	}
 
 }
