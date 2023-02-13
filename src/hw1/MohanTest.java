9d2599108e0adc69bb436f33ef7ee49517ef13de
Preethi Mohan <prmo2016@gmail.com>
2023-02-13 14:24:44 -0800

    updated 2:24pm

--- src/hw1/MohanTest.java
+++ src/hw1/MohanTest.java
@@ -3,14 +3,15 @@ import java.util.Date;
 
 public class MohanTest {
 	public static void main(String[] args) {
-		Student studentObject = new Student();
-		studentObject.setName("Preethi Mohan");
-		studentObject.setIntroduction("I am a second year CS major.");
-		studentObject.setStudentId(0304);
-		studentObject.setBranch(null);
+		String name = "Preethi Mohan";
+		String intro = "I am a second year CS major.";
+		int id = 304;
+		Branch theBranch = Branch.COMPUTER_SCIENCE;
 		Date theDate = new Date(2023, 2, 11);
-		studentObject.setDob(theDate); 
-		studentObject.toString();
+		
+		Student theStudent = new Student(name, intro, id, theBranch, theDate);
+		
+		System.out.println(theStudent.toString());
 	}
 }
 
