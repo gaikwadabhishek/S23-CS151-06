0f52543387eae671ee8d01884befd01ebd5da85b
Rio Taiga <53506239+Felty6@users.noreply.github.com>
2023-02-14 08:28:25 -0800

    Add files via upload

---
+++ src/hw1/TaigaTest.java
@@ -0,0 +1,42 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TaigaTest {
+	private final String name;
+	private final String studentId;
+	private final String branch; 
+	private final int year; 
+	
+	public TaigaTest(String name, String studentId, String branch, int year) {
+		this.name = name;
+		this.studentId = studentId;
+		this.branch = branch; 
+		this.year = year; 
+		
+		System.out.println("Name Registered: " + name);
+	}
+	
+	public TaigaTest(String name, String studentId, String branch) {
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getstudentId() {
+		return studentId;
+	}
+	
+	public String getBranch() {
+		return branch;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public static void main(String[] args) {
+		TaigaTest studentOne = new TaigaTest("Rio", "014163604", "Computer Science", 3);
+	}
+}
