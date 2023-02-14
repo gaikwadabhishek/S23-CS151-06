203666fabfe9ec56cbc38b9eebd0011a7e884431
bfiloteo <83046619+bfiloteo@users.noreply.github.com>
2023-02-14 15:35:48 -0800

    Update Student.java

--- src/hw1/Student.java
+++ src/hw1/Student.java
@@ -1,14 +1,12 @@
 package hw1;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-enum Branch {
-	COMPUTER_SCIENCE, SOFTWARE_ENGINEERING, OTHER
-}
-
 public class Student {
+
 	private String name;
 	private String introduction;
 	private int studentId; // only last 4 digits
@@ -71,18 +69,24 @@ public class Student {
 		return "Student [name=" + name + ", introduction=" + introduction + ", studentId=" + studentId + ", branch="
 				+ branch + ", dob=" + dob + "]";
 	}
+}
 
-	public class FiloteoTest {
 
-		public static void main(String[] args) throws ParseException {
-			// Date class referenced from https://stackoverflow.com/questions/8459776/date-in-constructor-in-java
-			DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
-			Date d1 = df.parse("08-07-2003"); 
+enum Branch {
+	COMPUTER_SCIENCE, SOFTWARE_ENGINEERING, OTHER
+}
+
+public class FiloteoTest {
+
+	public static void main(String[] args) throws ParseException {
+		// Date class referenced from https://stackoverflow.com/questions/8459776/date-in-constructor-in-java
+		DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+		Date d1 = df.parse("08-07-2003"); 
 		
-			Student me = new Student("Benjamin", "I like sleeping", 9335, Branch.OTHER, d1);
-			System.out.println(me.toString());
+		Student me = new Student("Benjamin", "I like sleeping", 9335, Branch.OTHER, d1);
+		System.out.println(me.toString());
 	}
 
 }
 
-}
+
