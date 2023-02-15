c5fb109a49c05afe7977a48cc9204a1ed758ef1f
James Kim <73400219+shallowsmith@users.noreply.github.com>
2023-02-14 19:37:10 -0800

    added hw1 q1

--- src/hw1/KimTest.java
+++ src/hw1/KimTest.java
@@ -17,7 +17,7 @@ public class KimTest {
 	// Reference: https://www.geeksforgeeks.org/java-simpledateformat-set-1/
 	// Created a method to format the date but realized this would not print the
 	// date in format because of the specified toString method, but keeping the 
-	// method for the date input for argument.
+	// method for the date input for argument
 	public static Date inFormat(String dob) {
 		String pattern = "MM-dd-yyyy";
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
